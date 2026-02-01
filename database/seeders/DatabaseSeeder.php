ee7077e6c0ebc2c44cdb623357e2c093d2212570
ap20231053 <ap20231053@student.fon.bg.ac.rs>
2026-02-01 20:45:30 +0100

    Izmenjena migracija,dodati factory i seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,22 +1,41 @@
-<?php
-
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Application;
+use App\Models\Company;
+use App\Models\Job;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Kreiranje test korisnika
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Kreiranje admin korisnika
+        User::factory()->count(5)->create(['role' => 'admin']);
+
+        // Kreiranje kompanija
+        Company::factory()->count(30)->create();
+
+        // Kreiranje poslova
+        Job::factory()->count(80)->create();
+
+        // Kreiranje aplikacija
+        Application::factory()->count(300)->create();
+
+        // Kreiranje studenata
+        User::factory()->count(30)->create(['role' => 'student']);
+
+        // Kreiranje alumni korisnika
+        User::factory()->count(30)->create(['role' => 'alumni']);
     }
 }
