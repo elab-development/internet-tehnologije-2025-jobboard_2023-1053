ef29964683decce3353dc8464401a1b2b756d7d6
ap20231053 <ap20231053@student.fon.bg.ac.rs>
2026-02-02 15:35:53 +0100

    Dodati factories i seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,3 +1,5 @@
+<?php
+
 namespace Database\Seeders;
 
 use App\Models\Application;
@@ -14,28 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Kreiranje test korisnika
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
-        // Kreiranje admin korisnika
         User::factory()->count(5)->create(['role' => 'admin']);
-
-        // Kreiranje kompanija
         Company::factory()->count(30)->create();
-
-        // Kreiranje poslova
         Job::factory()->count(80)->create();
-
-        // Kreiranje aplikacija
         Application::factory()->count(300)->create();
-
-        // Kreiranje studenata
         User::factory()->count(30)->create(['role' => 'student']);
-
-        // Kreiranje alumni korisnika
         User::factory()->count(30)->create(['role' => 'alumni']);
     }
 }
