7db503eabad004280e469cdfef70c9d35d0b20b7
damjana77 <di20231054@student.fon.bg.ac.rs>
2026-02-03 12:17:12 +0100

    Dodata logika sa rutama i funkcijama za User

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,12 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 Route::post('/register', [AuthController::class, 'register'] );
 Route::post('/login', [AuthController::class, 'login'] );
+Route::middleware('auth:sanctum,role:admin')->group(function () {
+
+});
+Route::middleware('auth:sanctum,role:company')->group(function () {
+    Route::get("/users/{role}/role",[UserController::class,'getUsersForRole']);
+});
+Route::middleware('auth:sanctum,role:student,alumni')->group(function () {
+
+});
