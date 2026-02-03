7910bc6e52ee217e3fd27219a3c15040e166f642
damjana77 <di20231054@student.fon.bg.ac.rs>
2026-02-03 21:29:08 +0100

    Dodate neke metode za rad sa company

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CompanyController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,11 +12,18 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [AuthController::class, 'register'] );
 Route::post('/login', [AuthController::class, 'login'] );
 Route::middleware('auth:sanctum,role:admin')->group(function () {
-
+ Route::delete("/user/delete/{user}", [UserController::class, 'destroy'] );
+    Route::delete("/company/delete/{company}", [CompanyController::class, 'destroy'] );
 });
 Route::middleware('auth:sanctum,role:company')->group(function () {
     Route::get("/users/{role}/role",[UserController::class,'getUsersForRole']);
+        Route::put("/company/update/{company}",[CompanyController::class,'update']);
 });
 Route::middleware('auth:sanctum,role:student,alumni')->group(function () {
+
+});
+
+Route::middleware('auth:sanctum,role:student,alumni,admin')->group(function () {
+    Route::get("/companies/name",[CompanyController::class,'searchCompany']);
 
 });
