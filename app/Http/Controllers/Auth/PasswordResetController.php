bcd109a5f700aaeaffd7de3b2decd3f9df50b24a
boris321p <bpadorev@gmail.com>
2026-02-04 12:39:14 +0100

    Dodato resetovanje lozinke

--- app/Http/Controllers/Auth/PasswordResetController.php
+++ app/Http/Controllers/Auth/PasswordResetController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
