2c7d6d832edb25f895cb6808864efe9d1960597d
ap20211053 <ap20231053@student.fon.bg.ac.rs>
2026-02-22 16:52:48 +0100

    Dodat Middleware

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'api/*',          // SPA frontend zahtevi
+        'webhook/receive' // primer treće strane
     ];
 }
