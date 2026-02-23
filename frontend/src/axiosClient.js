542e32ae40470e8a33e92a91ce3e8a62b2545ed3
ap20211053 <ap20231053@student.fon.bg.ac.rs>
2026-02-23 14:53:41 +0100

    Manja izmena

--- frontend/src/axiosClient.js
+++ frontend/src/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosClient = axios.create({
-    baseURL: "http://127.0.0.1:8000/api",
+    baseURL: "http://backend:8000/api",  // << promenjeno
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
