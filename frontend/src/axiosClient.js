c1bde74a1dc940df4eabfaf2fa157a8269384d53
ap20231053 <ap20231053@student.fon.bg.ac.rs>
2026-02-24 16:59:46 +0100

    Update axiosClient.js

--- frontend/src/axiosClient.js
+++ frontend/src/axiosClient.js
@@ -2,10 +2,11 @@ import axios from "axios";
 const axiosClient = axios.create({
     baseURL: "https://internet-tehnologije-2025-jobboard2023-1053-production-58f6.up.railway.app/api",  // << promenjeno
     headers: {
-        
+                "Content-Type": "application/json",
+
         Accept: "application/json",
     },
-    withCredentials: true,
+    withCredentials: false,
 });
 axiosClient.interceptors.request.use((config) => {
     const token = localStorage.getItem("ACCESS_TOKEN");
